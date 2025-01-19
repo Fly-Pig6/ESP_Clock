c08b9ca761007248f763a42a9771b3ad98ce1c3b
Fly-Pig6 <40312834+Fly-Pig6@users.noreply.github.com>
2025-01-19 19:58:55 +0800

    Add files via upload

--- ESP_Clock/ESP_Clock.ino
+++ ESP_Clock/ESP_Clock.ino
@@ -1,5 +1,15 @@
+/* bilibili@可爱的狗子汪
+ */
+
+#define XIAO_ESP32C3_USED  // 如果编译错误提示缺少库, 注释掉这一行重新编译
+
 #include <WiFi.h>
+#ifndef XIAO_ESP32C3_USED
+#include <NetworkClientSecure.h>
+#else
 #include <WiFiClientSecure.h>
+#endif
+
 #include <ArduinoJson.h>
 #include <TFT_eSPI.h>
 #include <RBD_Timer.h>
@@ -10,24 +20,37 @@
 #define DATE (1 << 2)
 #define FORECAST (1 << 3)
 
-#define WIFI_SSID "<WiFi名称>"
-#define WIFI_PASS "<WiFi密码>"
+/************** 按需要修改以下部分 *********************/
+
+#define WIFI_SSID "< WiFi_SSID >"  // <- ! 必须修改该字串
+#define WIFI_PASS "< WiFi密码 >"   // <- ! 必须修改该字串
 #define UTC_OFFSET 8
 #define UPDATE_INTERVAL_M 15
 
+/*
+ * [心知天气 - 登录](https://www.seniverse.com/login)
+ * [心知天气 - 控制台](https://www.seniverse.com/dashboard)
+ */
+
 const char* ntpServer = "ntp.aliyun.com";
 const char* host = "api.seniverse.com";
-const String key = "<心知天气KEY>";
-const String location = "<城市ID>";
+const String key = "< API_KEY >";        // <- ! 必须修改该字串
+const String location = "< 所在城市 >";  // <- ! 必须修改该字串
 
+/*****************************************************/
+#ifndef XIAO_ESP32C3_USED
+NetworkClientSecure client;
+#else
 WiFiClientSecure client;
+#endif
+
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite spr = TFT_eSprite(&tft);
 TFT_eSprite st = TFT_eSprite(&tft);
 
-RBD::Timer timer1(60000 * UPDATE_INTERVAL_M);  // 获取心知天气信息计时器
+RBD::Timer timer1(60000 * UPDATE_INTERVAL_M);  // 获取心知天气信息间隔
 RBD::Timer timer2(10000);                      // 滚动文字的驻屏显示时间
-RBD::Timer timer3(25);                         // 限制滚动文字滚动太快
+RBD::Timer timer3(25);                         // 限制滚动文字滚动太快, 每一帧与上一帧的间隔时间
 
 typedef struct {
   String text;
@@ -56,26 +79,20 @@ void setup() {
   tft.begin();
   tft.setRotation(3);
   tft.fillScreen(TFT_BLUE);
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(2);
-  tft.print("Connecting WiFi.. ");
+  tft.println("Please wait...");
 
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) delay(1500);
-  tft.println("OK");
 
-  client.setInsecure();  // HTTPS
+  client.setInsecure();  // HTTPS不安全连接
 
-  tft.print("Booting.. ");
   timer1.restart();  // 计时器
   timer2.restart();
   timer3.restart();
   configTime(3600 * UTC_OFFSET, 0, ntpServer);  // NTP服务器
   updateForecastDaily();
   updateWeather();
-  tft.println("DONE");
-  delay(2000);
 
   tft.fillScreen(TFT_BLACK);
   spr.setColorDepth(8);
@@ -88,12 +105,15 @@ void setup() {
   st.setTextDatum(TC_DATUM);
 }
 
+/*
+ * void loop()
+ */
 void loop() {
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
+  if (!getLocalTime(&timeinfo)) {  // 获取NTP时间
     return;
   }
-  // 更新时间并显示在屏幕上
+  // 更新时间刷新时间文字
   strftime(t, 9, "%X", &timeinfo);
   if (strcmp(ot, t) != 0) {
     strcpy(ot, t);
@@ -115,9 +135,13 @@ void loop() {
   if (timer2.onRestart())
     canScroll = true;
 
-  display();
+  display();  // 刷新显示
 }
 
+/*
+ * 根据API返回的JSON数据中的`code`字段, 绘制相应的天气图标
+ * "bmp.h"中修改对应图标或者添加更多天气图标!
+ */
 const uint8_t* toIconBitmap(int codeNum) {
   switch (codeNum) {
     case 0: return _0;  // 晴
@@ -140,24 +164,27 @@ void set_stext() {
   stext[2] = (WiFi.localIP()).toString();
 }
 
+/* 
+ * 在屏幕上绘制图像, 文字等
+ */
 void display() {
-  // 刷新前重置视窗, 避免出现位置错误
   tft.resetViewport();
-  // 绘制组件框架
+  // 圆角矩形框架
   tft.drawRoundRect(5, 5, 310, 120, 9, 0x39E7);
   tft.drawRoundRect(5, 130, 100, 105, 9, 0x39E7);
   tft.drawRoundRect(110, 130, 100, 105, 9, 0x39E7);
   tft.drawRoundRect(215, 130, 100, 105, 9, 0x39E7);
 
-  // 绘制时钟和滚动文字组件
+  // 滚动文字
   tft.setViewport(5, 5, 310, 120);
   tft.drawXBitmap(-10, 82, clock48x48, 48, 48, 0x39E7);
   st.pushSprite(25, 14);
-
+  // 文字滚动效果
   if (timer3.onRestart() && canScroll) {
     st.scroll(0, -1);
     tcount--;
   }
+  // 复位文字
   if (tcount <= 0) {
     tcount = 17;
     canScroll = false;
@@ -165,7 +192,7 @@ void display() {
     st.drawString(stext[stextIndex], 130, 0, 2);
     stextIndex = (stextIndex < 2 ? stextIndex + 1 : 0);
   }
-
+  // 时间文字
   if (TIME & updateThis) {
     spr.fillSprite(TFT_BLACK);
     spr.setTextDatum(MC_DATUM);
@@ -175,20 +202,20 @@ void display() {
     spr.unloadFont();
     spr.pushSprite(25, 36);
   }
+  // 日期文字
   if (DATE & updateThis) {
     tft.setTextDatum(BC_DATUM);
     tft.setTextSize(2);
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.drawString(String(wday) + " " + String(date), 155, 110);
   }
-  // 绘制天气组件
+  // 绘制底部天气栏
   if (FORECAST & updateThis) {
     tft.resetViewport();
     tft.fillRect(5, 130, 310, 105, TFT_BLACK);
     for (int i = 0; i < 3; i++) {
       tft.resetViewport();
       tft.setViewport(5 + 105 * i, 130, 100, 105);
-      // tft.frameViewport(TFT_WHITE, 1);
       tft.drawXBitmap(26, 10, toIconBitmap(day[i].code), WIDTH, HEIGHT, TFT_WHITE);
 
       tft.drawXBitmap(7, 62, thermometer16x16, 16, 16, TFT_SKYBLUE);
@@ -201,9 +228,12 @@ void display() {
       tft.drawString(String(day[i].humidity) + " %", 30, 84, 2);
     }
   }
-  updateThis = 0x00;  // 停止刷新
+  updateThis = 0x00;  // 暂停刷新
 }
 
+/*
+ * 向指定地址发送GET请求, 并跳过响应头
+ */
 bool sendRequest(const char* host, String path) {
   if (!client.connect(host, 443)) {
     return false;
@@ -224,6 +254,9 @@ bool sendRequest(const char* host, String path) {
   return true;
 }
 
+/*
+ * 更新当前天气数据
+ */
 void updateWeather() {
   if (!sendRequest(
         host, "/v3/weather/now.json?key=" + key + "&location=" + location + "&language=en")) {
@@ -244,6 +277,9 @@ void updateWeather() {
   client.stop();
 }
 
+/*
+ * 更新三日天气数据
+ */
 void updateForecastDaily() {
   if (!sendRequest(
         host, "/v3/weather/daily.json?key=" + key + "&location=" + location + "&language=en")) {
